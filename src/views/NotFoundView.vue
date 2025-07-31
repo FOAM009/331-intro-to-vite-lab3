794a9739405164cd5335b721375a68d3ce231cdc
FOAM <Chonticha_kumma@cmu.ac.th>
2025-08-01 01:34:53 +0700

    fix deploy (pls pass)

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script setup lang="ts">
-    import { DefineProps, withDefaults } from 'vue';
+    import { withDefaults } from 'vue';
     const props = withDefaults(
         defineProps<{
             resource: String
